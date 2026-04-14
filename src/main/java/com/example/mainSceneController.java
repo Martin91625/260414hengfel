9613bd20ac7c688a8ba224489bd1a7a2363c1ffd
El Miri Martin <elmiri.martin@gmail.com>
2026-04-14 16:30:23 +0200

    kész

--- src/main/java/com/example/mainSceneController.java
+++ src/main/java/com/example/mainSceneController.java
@@ -4,6 +4,7 @@ import java.util.InputMismatchException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 public class mainSceneController {
@@ -24,18 +25,41 @@ public class mainSceneController {
 
     void startCalc() {
         this.testInput();
-        double radius = Double.parseDouble(radiusField.getText());
-        double height = Double.parseDouble(heightField.getText());
-        double surface = Cylinder.calcSurface(radius, height);
-        
-        surfaceField.setText(String.valueOf(surface));
+
+        try {
+            double radius = Double.parseDouble(radiusField.getText());
+            double height = Double.parseDouble(heightField.getText());
+            double surface = Cylinder.calcSurface(radius, height);    
+            surfaceField.setText(String.valueOf(surface));
+        } catch (InputMismatchException e) {
+            String msg = e.getMessage();
+            showMsg(msg);
+            throw new InputMismatchException(msg);
+        }
     }
 
     void testInput() {
         String radiusStr = radiusField.getText();
         String heightStr = heightField.getText();
         if (radiusStr.isEmpty() || heightStr.isEmpty()) {
-            throw new InputMismatchException("Hiba! A mezők nem lehetnek üresek!");            
+            String msg = "Hiba! A mezők nem lehetnek üresek!";
+            this.showMsg(msg);
+            throw new InputMismatchException(msg);            
         }
+        if (!radiusStr.matches("-?[0.-9.]+") || !heightStr.matches("-?[0.-9.]+")) {
+            String msg = "Hiba! A bemenet csak valós szám lehet";
+            this.showMsg(msg);
+            throw new InputMismatchException(msg);
+        }    
+    }
+
+    void showMsg(String msg) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setContentText(msg);
+        alert.setHeaderText("Hibás bemenet.");
+        alert.setTitle("Hiba");
+        alert.initOwner(App._stage);
+        alert.showAndWait();
     }
 }
+
